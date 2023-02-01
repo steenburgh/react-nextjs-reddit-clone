97ea146ac7de135c7ed5b86cc0492cfa6a80008b
Duncan Steenburgh <duncan.steenburgh@gmail.com>
2023-02-01 01:37:55 -0600

    Introduce linter warning to see what happens on build

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect, useRef } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
+	const someRef = useRef(null);
+	useEffect(() => {
+		return () => {
+			console.log(someRef.current)
+		}
+	})
 	return <Component {...pageProps} />
 }
