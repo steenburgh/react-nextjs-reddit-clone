658e969fd07b629ea3e5fa3ab3e959ca21a4bbb7
Duncan Steenburgh <duncan.steenburgh@gmail.com>
2023-02-01 02:08:08 -0600

    Remove test linter warning

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,5 @@ import type { AppProps } from 'next/app'
 import { useEffect, useRef } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
-	const someRef = useRef(null);
-	useEffect(() => {
-		return () => {
-			console.log(someRef.current)
-		}
-	})
 	return <Component {...pageProps} />
 }
