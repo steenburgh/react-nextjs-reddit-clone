60e5803496a748011df826efff6de11ebdd936c0
Duncan Steenburgh <duncan.steenburgh@gmail.com>
2023-02-07 03:02:42 -0600

    Remove testing code, oops :|

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -4,7 +4,7 @@ const fetcher = async <JSON = any>(
 	input: RequestInfo,
 	init?: RequestInit
 ): Promise<JSON> => {
-	await delay(3000); // TODO: Remove in prod
+	// await delay(3000); // TODO: Remove in prod
 	// if (fail) {
 	// 	fail = !fail;
 	// 	throw Error("Debug: Intentionally failed");
